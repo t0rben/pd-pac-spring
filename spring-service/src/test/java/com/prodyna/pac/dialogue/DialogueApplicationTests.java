ddfb933162af2115ace6b251640ac646c04abc3c
Torben Bock <torben.bock@prodyna.com>
2019-03-05 13:47:47 +0100

    Initial setup

---
+++ spring-service/src/test/java/com/prodyna/pac/dialogue/DialogueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prodyna.pac.dialogue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DialogueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
